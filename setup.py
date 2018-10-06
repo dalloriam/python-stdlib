6d3541c064b9350f3470cb168b3d09f46bc14bef
Dalloriam <dalloriam@gmail.com>
2018-10-06 14:43:33 -0400

    Un-break dependencies

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     license='MIT',
     description='Personal standard library.',
     long_description=readme,
-    install_requires=['flask']
+    install_requires=['requests', 'flask']
 )
